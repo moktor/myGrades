07e1f67564976b993ff226fcaa78834483c57db5
tarnschaf <tarnschaf@gmx.com>
2011-06-25 17:43:21 +0200

    Student in db eintragen

--- src/controller/studentCtrl.java
+++ src/controller/studentCtrl.java
@@ -1,23 +1,42 @@
 package controller;
+import java.io.Serializable;
 
+import model.Student;
 
+import javax.ejb.EJB;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
-
+import business.*;
 
 
 
 @Named
 @SessionScoped
-public class studentCtrl {
+public class studentCtrl implements Serializable {
 
+	@EJB
+	DbPerson a;
+	
+	public studentCtrl(){
+		
+	}
+	
+	
+	public String createStudent(){
+		a.createStudent();
+		return "addStudent";
+	}
+	
+	
 	
 	
 	public String test(){
 		
 		
-		return "dinge";
+		return "delStudent";
 	}
+	
+	
 	
 	
 	

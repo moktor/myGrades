6a3d272da04af67a974da9cb65c25a8a1f21f88a
Ludwig Scholere <LudwigScholer@googlemail.com>
2011-06-28 14:57:11 +0200

    Ludwigs commit

---
+++ src/business/DbPerson.java
@@ -0,0 +1,38 @@
+package business;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import model.*;
+
+
+@Stateless
+public class DbPerson {
+
+	
+    @PersistenceContext
+    private EntityManager em;
+	
+	
+    public DbPerson() {
+		// TODO Auto-generated constructor stub
+	}
+    
+    
+    public void createStudent(
+    String gender, String firstname, String lastname, String adresse,
+    String email, String phone, String mobil, String keyword){
+    	
+    	
+    	Student s = new Student( gender, firstname, lastname, adresse, email, phone, mobil, keyword);
+    	em.persist(s);
+    }
+    
+    public List<Student> getAllStudents(){
+    	return em.createNamedQuery(Student.FIND_ALL, Student.class).getResultList();
+    }
+    
+    
+}

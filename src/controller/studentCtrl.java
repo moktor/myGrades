f2b2b3ffaf1496f1712e94f4b841bc10de56d678
tarnschaf <tarnschaf@gmx.com>
2011-07-06 01:04:19 +0200

    search method improved

--- src/controller/studentCtrl.java
+++ src/controller/studentCtrl.java
@@ -102,9 +102,13 @@ public class studentCtrl implements Serializable {
 	//additional test logger for view of all students
 	public List<Student> getAllStudents(){
 		List<Student> list = dbP.getAllStudents();
-		
-		
-		
+		studentList = sortList(list);
+		return list;
+	}
+	
+	// --------------------- FM ------------------------sortStudents----------------------
+	
+	public List<Student> sortList(List<Student> list){
 		if(sortId){
 			list = dbP.sortById(list);
 		}
@@ -120,11 +124,8 @@ public class studentCtrl implements Serializable {
 		if(sortAdress){
 		list = dbP.sortByAdress(list);
 		}
-
-		studentList = list;
 		return list;
 	}
-	// --------------------- FM ------------------------sortStudents----------------------
 	
 	
 	public void sortStudentsById(){
